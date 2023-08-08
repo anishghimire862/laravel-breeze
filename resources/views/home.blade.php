2b5c1082fc7845822f9ad8c523eab8ce87eaf3a9
Anish Ghimire <anishghimire862@gmail.com>
2023-08-08 12:01:10 +0545

    check config

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,7 +24,7 @@
                         <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                             <div class="p-6 text-gray-900 dark:text-gray-100">
                                 {{ __("You're welcome -- guest!") }}
-                                {{ env('APP_NAME') }}
+                                {{  config('app.name') }}
                             </div>
                         </div>
                     </div>
